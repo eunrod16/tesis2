94a3b0a66db88d628a309f382b3e7a9d190ea98f
Eunice <hildaeunicer@hotmail.com>
2017-02-11 11:49:48 -0600

    codigo comentado

---
+++ www/js/index.js
@@ -0,0 +1,11 @@
+$("#btn-play").on("click",function() {
+  window.location = "categories.html";
+});
+
+$("#btn-help").on("click",function() {
+  window.location = "help.html";
+});
+
+$("#btn-loose").on("click",function() {
+  window.location = "loose.html";
+});
